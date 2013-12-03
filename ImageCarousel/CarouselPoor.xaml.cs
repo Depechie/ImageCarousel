412ba6f2c336556bc7aece538e4e9bdd528a2fba
Depechie <glenn.versweyveld@gmail.com>
2013-12-03 12:53:20 +0100

    Small design tweaks

--- ImageCarousel/CarouselPoor.xaml.cs
+++ ImageCarousel/CarouselPoor.xaml.cs
@@ -64,11 +64,6 @@ namespace ImageCarousel
                 this.ButtonRight.Visibility = Visibility.Visible;
         }
 
-        private void LoadImages()
-        {
-            _images.ForEach(item => this.ImageList.Children.Add(item));
-        }
-
         private void InitImages()
         {
             _images.Add(new Image() { Source = new BitmapImage(new Uri(@"/Images/sink1.jpg", UriKind.Relative)), Width = 480 });
@@ -76,6 +71,11 @@ namespace ImageCarousel
             _images.Add(new Image() { Source = new BitmapImage(new Uri(@"/Images/sink3.jpg", UriKind.Relative)), Width = 480 });
             _images.Add(new Image() { Source = new BitmapImage(new Uri(@"/Images/sink4.jpg", UriKind.Relative)), Width = 480 });
             _images.Add(new Image() { Source = new BitmapImage(new Uri(@"/Images/sink5.jpg", UriKind.Relative)), Width = 480 });
+        }
+
+        private void LoadImages()
+        {
+            _images.ForEach(item => this.ImageList.Children.Add(item));
         }
 
         private void ButtonLeft_Click(object sender, RoutedEventArgs e)
